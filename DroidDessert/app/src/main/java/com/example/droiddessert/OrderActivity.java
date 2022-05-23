cffa15f647b291a241503b6ada13395fc50b5424
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-05-23 10:13:12 +0700

    Create radiobutton and spinner control

--- DroidDessert/app/src/main/java/com/example/droiddessert/OrderActivity.java
+++ DroidDessert/app/src/main/java/com/example/droiddessert/OrderActivity.java
@@ -2,13 +2,86 @@ package com.example.droiddessert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        //Create the spinner
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if(spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+        // Create ArrayAdapter using the string array and default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_spinner_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+        // ... End of onCreate code ...
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    public void onRadioButtonClicked(View view) {
+        //Is the button now checked ?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.sameday: {
+                if(checked) {
+                    //Same day service
+                    displayToast(getString(R.string.same_day_msg));
+                }
+                break;
+            }
+
+            case R.id.nextday: {
+                if (checked) {
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_msg));
+                }
+                break;
+            }
+
+            case R.id.pickup: {
+                if (checked) {
+                    // Pick up
+                    displayToast(getString(R.string.pick_up_msg));
+                }
+                break;
+            }
+
+            default:
+                // Do nothing.
+                break;
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        //do nothing
     }
 }
